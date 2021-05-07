979614afbcb16e515342d1cbdbf4802e62a74873
Seiya Asahina <seiya@asahina>
2021-05-07 14:20:07 +0900

    fileupload add

---
+++ illust/views.py
@@ -0,0 +1,67 @@
+from django.shortcuts import render,redirect
+
+## Create your views here.
+#from django.shortcuts import render
+from django.views import View
+
+from .models import Design
+from .forms import DesignForm
+
+
+#ファイルのMIMEタイプを調べるためのpython-magicをインポート
+import magic
+
+#許可するMIMEタイプのリスト
+ALLOWED_MIME    = [ "application/pdf" ]
+
+
+
+class illustView(View):
+
+    def get(self, request, *args, **kwargs):
+
+        #Designクラスを使用し、DBへアクセス、データ全件閲覧
+        designs = Design.objects.all()
+
+        button1     = "Prev"
+        data        = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+        category    = "カテゴリ−１"
+        category2   = "カテゴリ−2"
+        category3   = "カテゴリ−3"
+
+        context = {
+                   "button1":button1,
+                   "data":data,
+                   "category1":category,
+                   "category2": category2,
+                   "category3": category3,
+
+                   "designs":designs,
+
+                   }
+
+        return render(request,"illust/index.html",context)
+
+    def post(self, request, *args, **kwargs):
+
+
+        #フォームクラスに、request.POST、request.FILESを引数に
+        form = DesignForm(request.POST, request.FILES)
+
+        #python-magicで投稿されたファイルのMIMEタイプを調べる
+        mime_type   = magic.from_buffer(request.FILES["file"].read(1024) , mime=True)
+
+        if form.is_valid():
+            print("バリデーションOK ")
+
+            #MIMEタイプが許可したMIMEタイプのリストに含まれるかチェック。あれば保存
+            if mime_type in ALLOWED_MIME:
+                form.save()
+            else:
+                print("このファイルは許可されていません")
+        else:
+            print("バリデーションNG")
+
+        return redirect("illust:index")
+
+index   = illustView.as_view()
